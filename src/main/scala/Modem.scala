af238dc577eafd5b9d5b86b93eea04311c85283f
Josh Sanz <jsanz@berkeley.edu>
2018-10-22 14:15:40 -0700

    Start filling in modem trait

---
+++ src/main/scala/Modem.scala
@@ -0,0 +1,25 @@
+package modem
+
+import chisel3._
+import chisel3.experimental.FixedPoint
+import chisel3.util._
+
+import dsptools.numbers._
+import freechips.rocketchip.diplomacy.LazyModule
+import freechips.rocketchip.subsystem.BaseSubsystem
+
+/**
+  * Mixin for top-level rocket to add a modem
+  *
+  */
+trait HasPeripheryModem extends BaseSubsystem {
+  // instantiate chain blocks
+  val cordicChain = LazyModule(new CordicThing(FixedCordicParams(8, 10)))
+  val CFOChain = LazyModule(new CFOThing(FixedCFOParams(8, 10)))
+  val packetDetectChain = LazyModule(new PacketDetectThing(FixedPacketDetectParams(8, 10)))
+  val equalizerChain = LazyModule(new EqualizerThing(FixedEqualizerParams(8, 10)))
+  // connect memory interfaces to pbus
+  pbus.toVariableWidthSlave(Some("cordicWrite")) { cordicChain.writeQueue.mem.get }
+  pbus.toVariableWidthSlave(Some("cordicRead")) { cordicChain.readQueue.mem.get }
+}
+
