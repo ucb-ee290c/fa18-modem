f08615ad2687fe5d1d7b8000e8007a68f11a5d59
Josh Sanz <jsanz@berkeley.edu>
2018-10-22 23:50:32 -0700

    Start implementing correlator

--- src/main/scala/pkt_detect/PacketDetect.scala
+++ src/main/scala/pkt_detect/PacketDetect.scala
@@ -15,10 +15,12 @@ import freechips.rocketchip.subsystem.BaseSubsystem
  */
 trait PacketDetectParams[T <: Data] {
   val protoIQ: T
-  val powerThresh: Boolean
+  val powerThreshVal: Double // Power threshold
+  val powerThreshWindow: Int // Number of samples greater than power in a row before triggering
   val correlationThresh: Boolean
-  val powerThreshVal: Double
   val correlationThreshVal: Double
+  val correlationWindow: Int // Number of strided correlations to sum
+  val correlationStride: Int // Stride between correlated samples
 }
 
 /**
@@ -27,8 +29,7 @@ trait PacketDetectParams[T <: Data] {
 case class FixedPacketDetectParams(
   // width of I and Q
   iqWidth: Int,
-  // enable power thresholding?
-  powerThresh: Boolean = true,
+  powerThreshWindow: Int = 4,
   // enable correlation thresholding?
   correlationThresh: Boolean = false
 ) extends PacketDetectParams[FixedPoint] {
@@ -36,7 +37,9 @@ case class FixedPacketDetectParams(
   // binary point is iqWidth-2 to allow for some inflation
   val protoIQ = FixedPoint(iqWidth.W, (iqWidth-2).BP)
   val powerThreshVal = 0.75
-  val correlationThreshVal = 0.9
+  val correlationThreshVal = 0.75
+  val correlationWindow = 4
+  val correlationStride = 16
 }
 
 /**
@@ -67,6 +70,7 @@ object PacketDetectIO {
     new PacketDetectIO(params)
 }
 
+
 /**
   * PacketDetector
   * Takes IQ samples and uses some combination of power and correlation thresholding to detect the start and end
@@ -74,9 +78,25 @@ object PacketDetectIO {
   */
 class PacketDetect[T <: Data : Real : BinaryRepresentation](params: PacketDetectParams[T]) {
   val io = IO(PacketDetectIO(params))
-  val dataShift = ShiftRegister(io.in.bits, n=16, resetData=Real[T].zero, en=io.in.valid)
-  val validShift = ShiftRegister(io.in.valid, n=16, resetData=false.B)
-  
+  val windowSize = params.correlationStride + params.correlationWindow
+  val dataVec = Vec.fill(windowSize)(RegInit(Real[T].zero)).foldLeft(io.in.bits){ case (a, b) => a := b; b }
+  val validShift = ShiftRegister(io.in.valid, n=windowSize, resetData=false.B)
+
+  // Correlation Threshold
+  val corrComp := Wire(false.B)
+  if (params.correlationThresh) {
+    // Squared magnitude of sum of correlations
+    val corrNumTmp = (0 until params.correlationWindow).map(i => dataVec(i) * dataVec(i + params.correlationStride).conjugate).reduce(_ + _)
+    val corrNumVal = (corrNumTmp * corrNumTmp.conjugate).real
+    // Squared magnitude
+    val corrDenomVal = (0 until params.correlationWindow).map(i => dataVec(i) * dataVec(i).conjugate).reduce(_ + _).real
+    if (params.correlationThreshVal == 0.75) {
+      // Compare 0.75 numerate to denominator for the
+      corrComp := ((corrNumVal >> 1) + (corrNumVal >> 2)) > corrDenomVal * corrDenomVal
+    } else {
+      assert(false, "Correlation thresholds other than 0.75 not implemented")
+    }
+  }
 }
 
 
