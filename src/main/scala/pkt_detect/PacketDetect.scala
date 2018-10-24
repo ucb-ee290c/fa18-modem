f9bbb57a6552724f3bb3f1893107155b17d399e4
Josh Sanz <jsanz@berkeley.edu>
2018-10-24 14:34:36 -0700

    Passes power-only tests

--- src/main/scala/pkt_detect/PacketDetect.scala
+++ src/main/scala/pkt_detect/PacketDetect.scala
@@ -1,12 +1,10 @@
 package modem
 
 import chisel3._
-import chisel3.core.IO
 import chisel3.experimental.FixedPoint
 import chisel3.util._
 import dsptools.numbers._
-import freechips.rocketchip.diplomacy.LazyModule
-import freechips.rocketchip.subsystem.BaseSubsystem
+import breeze.math.Complex
 
 
 /**
@@ -14,7 +12,7 @@ import freechips.rocketchip.subsystem.BaseSubsystem
  * Type generic
  */
 trait PacketDetectParams[T <: Data] {
-  val protoIQ: T
+  val protoIQ: DspComplex[T]
   val powerThreshVal: Double // Power threshold
   val powerThreshWindow: Int // Number of samples greater than power in a row before triggering
   val correlationThresh: Boolean
@@ -35,10 +33,10 @@ case class FixedPacketDetectParams(
 ) extends PacketDetectParams[FixedPoint] {
   // prototype for iq
   // binary point is iqWidth-2 to allow for some inflation
-  val protoIQ = FixedPoint(iqWidth.W, (iqWidth-2).BP)
+  val protoIQ = DspComplex(FixedPoint(iqWidth.W, (iqWidth-2).BP))
   val powerThreshVal = 0.75
   val correlationThreshVal = 0.75
-  val correlationWindow = 4
+  val correlationWindow = powerThreshWindow
   val correlationStride = 16
 }
 
@@ -46,12 +44,30 @@ case class FixedPacketDetectParams(
   * Bundle type that describes the input, state, and output of PacketDetect
   */
 class PacketDetectBundle[T <: Data](params: PacketDetectParams[T]) extends Bundle {
-  val iq: T = params.protoIQ.cloneType
+  val iq: DspComplex[T] = params.protoIQ.cloneType
 
   override def cloneType: this.type = PacketDetectBundle(params).asInstanceOf[this.type]
 }
 object PacketDetectBundle {
   def apply[T <: Data](params: PacketDetectParams[T]): PacketDetectBundle[T] = new PacketDetectBundle[T](params)
+}
+
+/**
+  * Bundle type for debug data
+  */
+class PacketDetectDebugBundle[T <: Data](params: PacketDetectParams[T]) extends Bundle {
+  val powerHigh: Bool = Bool()
+  val powerLow: Bool = Bool()
+  val corrComp: Bool = Bool()
+  val flushing: Bool = Bool()
+  val iq: DspComplex[T] = params.protoIQ.cloneType
+  val cnt: UInt = UInt()
+
+  override def cloneType: this.type = PacketDetectDebugBundle(params).asInstanceOf[this.type]
+
+}
+object PacketDetectDebugBundle {
+  def apply[T <: Data](params: PacketDetectParams[T]): PacketDetectDebugBundle[T] = new PacketDetectDebugBundle[T](params)
 }
 
 /**
@@ -63,6 +79,8 @@ class PacketDetectIO[T <: Data](params: PacketDetectParams[T]) extends Bundle {
 
   val pktStart = Output(Bool())
 
+  val debug = Output(PacketDetectDebugBundle[T](params))
+
   override def cloneType: this.type = PacketDetectIO(params).asInstanceOf[this.type]
 }
 object PacketDetectIO {
@@ -70,33 +88,159 @@ object PacketDetectIO {
     new PacketDetectIO(params)
 }
 
+/**
+  * Correlation for packet detect
+  */
+class Correlator[T <: Data : Real : BinaryRepresentation](params: PacketDetectParams[T]) extends Module {
+  val windowSize = params.correlationStride + params.correlationWindow
+  val io = IO(new Bundle {
+    val data = Input(Vec(windowSize, params.protoIQ))
+    val correlated = Output(new Bool())
+  })
+  // Squared magnitude of sum of correlations
+  val corrNumVal = (0 until params.correlationWindow).map(
+      i => io.data(i) * io.data(i + params.correlationStride).conj()
+      ).reduce(_ + _).abssq()
+  // Squared magnitude (real)
+  val corrDenomVal = (0 until params.correlationWindow).map(i => io.data(i).abssq()).reduce(_ + _)
+  if (params.correlationThreshVal == 0.75) {
+    // Compare 0.75 numerate to denominator for the
+    val corrComp = ((corrNumVal >> 1) + (corrNumVal >> 2)) > corrDenomVal * corrDenomVal
+    io.correlated := corrComp
+  } else {
+    assert(false, "Correlation thresholds other than 0.75 not implemented")
+  }
+}
+object Correlator {
+  def apply[T <: Data : Real : BinaryRepresentation](data: Vec[DspComplex[T]], params: PacketDetectParams[T]): Bool = {
+    val correlator = Module(new Correlator(params))
+    correlator.io.data := data
+    correlator.io.correlated
+  }
+}
+
+/**
+  * Power thresholding for packet detect
+  */
+class PowerMeter[T <: Data : Real : BinaryRepresentation](params: PacketDetectParams[T]) extends Module {
+  val io = IO(new Bundle {
+    val data = Input(Vec(params.powerThreshWindow, params.protoIQ))
+    val powerHigh = Output(Bool())
+    val powerLow = Output(Bool())
+  })
+  val powers = (0 until params.powerThreshWindow).map(i => io.data(i).abssq()).map(p => p > Real[T].fromDouble(params.powerThreshVal))
+  io.powerHigh := powers.reduce(_ & _)
+  io.powerLow  := !powers.reduce(_ | _)
+}
+object PowerMeter {
+  def apply[T <: Data : Real : BinaryRepresentation](data: Vec[DspComplex[T]], params: PacketDetectParams[T]): PowerMeter[T] = {
+    val meter = Module(new PowerMeter(params))
+    meter.io.data := data
+    meter
+  }
+}
 
 /**
   * PacketDetector
   * Takes IQ samples and uses some combination of power and correlation thresholding to detect the start and end
   * of packets
   */
-class PacketDetect[T <: Data : Real : BinaryRepresentation](params: PacketDetectParams[T]) {
+class PacketDetect[T <: Data : Real : BinaryRepresentation](params: PacketDetectParams[T]) extends Module {
   val io = IO(PacketDetectIO(params))
-  val windowSize = params.correlationStride + params.correlationWindow
-  val dataVec = Vec.fill(windowSize)(RegInit(Real[T].zero)).foldLeft(io.in.bits){ case (a, b) => a := b; b }
-  val validShift = ShiftRegister(io.in.valid, n=windowSize, resetData=false.B)
 
-  // Correlation Threshold
-  val corrComp := Wire(false.B)
-  if (params.correlationThresh) {
-    // Squared magnitude of sum of correlations
-    val corrNumTmp = (0 until params.correlationWindow).map(i => dataVec(i) * dataVec(i + params.correlationStride).conjugate).reduce(_ + _)
-    val corrNumVal = (corrNumTmp * corrNumTmp.conjugate).real
-    // Squared magnitude
-    val corrDenomVal = (0 until params.correlationWindow).map(i => dataVec(i) * dataVec(i).conjugate).reduce(_ + _).real
-    if (params.correlationThreshVal == 0.75) {
-      // Compare 0.75 numerate to denominator for the
-      corrComp := ((corrNumVal >> 1) + (corrNumVal >> 2)) > corrDenomVal * corrDenomVal
-    } else {
-      assert(false, "Correlation thresholds other than 0.75 not implemented")
+  val windowSize = params.correlationStride + params.correlationWindow
+  val complexZero = Wire(params.protoIQ)
+  complexZero.real := Real[T].zero
+  complexZero.imag := Real[T].zero
+
+  // State machine states
+  val sFlushing :: sNoPkt :: sNoPktValid :: sPkt :: sPktValid :: Nil = Enum(5)
+  val state = RegInit(sFlushing)
+  val flushCounter = RegInit(0.U(8.W))
+  val isValid = state === sNoPktValid || state === sPktValid
+  val isPkt = state === sPkt || state === sPktValid
+
+  // Store inputs for processing
+  val iqBuf = RegNext(io.in.bits.iq)
+  val dataVec = Reg(Vec(windowSize, params.protoIQ))
+  when(state === sFlushing) {
+    dataVec(0) := complexZero
+    for (i <- 1 until windowSize) {
+      dataVec(i) := dataVec(i - 1)
     }
   }
+  .elsewhen(state === sPktValid || state === sNoPktValid) {
+    printf("bringing in new data\n")
+    dataVec(0) := iqBuf
+    for (i <- 1 until windowSize) {
+      dataVec(i) := dataVec(i - 1)
+    }
+  }
+  .otherwise {
+    for (i <- 0 until windowSize) {
+      dataVec(i) := dataVec(i)
+    }
+  }
+
+  // Power Threshold
+  val powerMeter = PowerMeter(Vec(dataVec.drop(dataVec.length - params.powerThreshWindow)), params)
+  val powerHigh = powerMeter.io.powerHigh
+  val powerLow = powerMeter.io.powerLow
+
+  // Correlation Threshold
+  val corrComp = Wire(Bool())
+  corrComp := true.B
+  if (params.correlationThresh) {
+    corrComp := Correlator(dataVec, params)
+  }
+
+  // State Update
+  val nextState = Wire(sFlushing.cloneType)
+  when (flushCounter < windowSize.U) {
+    nextState := sFlushing // Do an initial flush of the registers
+    flushCounter := flushCounter + 1.U
+  }.otherwise {
+    flushCounter := flushCounter
+    when(io.in.valid) {
+      when(powerHigh && corrComp) {
+        nextState := sPktValid
+      }.elsewhen(powerLow) {
+        nextState := sNoPktValid
+      }.elsewhen(isPkt) {
+        nextState := sPktValid
+      }.otherwise {
+        nextState := sNoPktValid
+      }
+    }.otherwise {
+      when(powerHigh && corrComp) {
+        nextState := sPkt
+      }.elsewhen(powerLow) {
+        nextState := sNoPkt
+      }.elsewhen(isPkt) {
+        nextState := sPkt
+      }.otherwise {
+        nextState := sNoPkt
+      }
+    }
+  }
+  state := nextState
+
+  // Output Logic
+  // pktStart goes high on transition from nopkt to pkt
+  val pktStartReg = Reg(Bool())
+  pktStartReg := (nextState === sPktValid || nextState === sPkt) && (state === sNoPkt || state === sNoPktValid)
+  io.pktStart := pktStartReg
+  io.in.ready := state =!= sFlushing
+  io.out.bits.iq := dataVec(dataVec.length - 1)
+  io.out.valid := (nextState === sPktValid)
+
+  // debug output
+  io.debug.corrComp := corrComp
+  io.debug.flushing := state === sFlushing
+  io.debug.powerHigh := powerHigh
+  io.debug.powerLow := powerLow
+  io.debug.iq := dataVec(windowSize-1)// dataVec(0) * dataVec(0).conj()
+  io.debug.cnt := flushCounter
 }
 
 
---
+++ src/test/scala/PacketDetectTester.scala
@@ -0,0 +1,108 @@
+package modem
+
+import breeze.math.Complex
+import dsptools.DspTester
+
+/**
+  * Case class holding information needed to run an individual test
+  */
+case class IQ(
+  // input iq vectors
+  iqin: Seq[Complex],
+  iqout: Option[Seq[Complex]] = None
+)
+
+/**
+  * DspTester for PacketDetect
+  *
+  * Run each trial in @trials
+  */
+class PacketDetectTester[T <: chisel3.Data](c: PacketDetect[T], trials: Seq[IQ], tolLSBs: Int = 2) extends DspTester(c) {
+  def getIQOut(c: PacketDetect[T], v: Vector[Complex]): Vector[Complex] = {
+    var vout = v
+    if (peek(c.io.out.valid)) {
+      vout = vout :+ peek(c.io.out.bits.iq)
+    }
+    vout
+  }
+
+  val maxCyclesWait = 32
+
+  poke(c.io.out.ready, 1)
+  poke(c.io.in.valid, 1)
+
+  for (trial <- trials) {
+    var iqOut = Vector[Complex]()
+    for (iq <- trial.iqin) {
+      poke(c.io.in.bits.iq, iq)
+      // wait until input is accepted
+      var cyclesWaiting = 0
+      while (!peek(c.io.in.ready) && cyclesWaiting < maxCyclesWait) {
+        cyclesWaiting += 1
+        if (cyclesWaiting >= maxCyclesWait) {
+          expect(false, "waited for input too long")
+        }
+        iqOut = getIQOut(c, iqOut)
+        step(1)
+      }
+      iqOut = getIQOut(c, iqOut)
+      peek(c.io.debug.powerHigh)
+      peek(c.io.debug.powerLow)
+      peek(c.io.debug.corrComp)
+//      peek(c.io.debug.iq)
+      step(1)
+    }
+    // wait for remaining output after pushing in IQ data
+    poke(c.io.in.valid, 1)
+    poke(c.io.in.bits.iq, Complex(0, 0))
+    var cyclesWaiting = 0
+    while (cyclesWaiting < maxCyclesWait) {
+      cyclesWaiting += 1
+      peek(c.io.debug.powerHigh)
+      peek(c.io.debug.powerLow)
+      peek(c.io.debug.corrComp)
+//      peek(c.io.debug.iq)
+      iqOut = getIQOut(c, iqOut)
+      step(1)
+    }
+    peek(c.io.debug.powerHigh)
+    peek(c.io.debug.powerLow)
+    peek(c.io.debug.corrComp)
+//    peek(c.io.debug.iq)
+    iqOut = getIQOut(c, iqOut)
+    // set desired tolerance
+    // in this case, it's pretty loose (2 bits)
+    // can you get tolerance of 1 bit? 0? what makes the most sense?
+    fixTolLSBs.withValue(tolLSBs) {
+      // check every output where we have an expected value
+      if (trial.iqout.isEmpty) {
+        assert(iqOut.isEmpty, "No IQ should have been passed through")
+      } else {
+        val iqRef = trial.iqout.get
+        assert(iqOut.length == iqRef.length,
+               s"The packet length was ${iqOut.length} but should have been ${iqRef.length}")
+        iqOut.indices.foreach {
+         i => assert(iqOut(i) == iqRef(i), s"iq mismatch: ref ${iqRef(i)} != ${iqOut(i)} @$i")}
+      }
+    }
+  }
+}
+
+/**
+  * Convenience function for running tests
+  */
+object FixedPacketDetectTester {
+  def apply(params: FixedPacketDetectParams, trials: Seq[IQ]): Boolean = {
+    chisel3.iotesters.Driver.execute(Array("-tbn", "firrtl", "-fiwv"), () => new PacketDetect(params)) {
+      c => new PacketDetectTester(c, trials)
+    }
+  }
+}
+
+object RealPacketDetectTester {
+  def apply(params: PacketDetectParams[dsptools.numbers.DspReal], trials: Seq[IQ]): Boolean = {
+    chisel3.iotesters.Driver.execute(Array("-tbn", "verilator", "-fiwv"), () => new PacketDetect(params)) {
+      c => new PacketDetectTester(c, trials)
+    }
+  }
+}
