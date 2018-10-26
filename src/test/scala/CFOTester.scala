1d0b8704785ae51fb85a19eb109dc0aa4145ca48
sehuang <sehuang@berkeley.edu>
2018-10-26 10:54:47 -0700

    Very rudimentary CFO tester

--- src/test/scala/CFOTester.scala
+++ src/test/scala/CFOTester.scala
@@ -13,82 +13,26 @@ case class IQ(
 )
 
 /**
-  * DspTester for PacketDetect
+  * DspTester for CFO
   *
   * Run each trial in @trials
   */
-class PacketDetectTester[T <: chisel3.Data](c: PacketDetect[T], trials: Seq[IQ], tolLSBs: Int = 2) extends DspTester(c) {
-  def getIQOut(c: PacketDetect[T], v: Vector[Complex]): Vector[Complex] = {
-    var vout = v
-    if (peek(c.io.out.valid)) {
-      vout = vout :+ peek(c.io.out.bits.iq)
-    }
-    vout
-  }
-
-  val maxCyclesWait = 32
-
+class CFOTester[T <: chisel3.Data](c: CFOCorrection[T], trials: Seq[IQ], tolLSBs: Int = 2) extends DspTester(c) {
   poke(c.io.out.ready, 1)
   poke(c.io.in.valid, 1)
 
-  for (trial <- trials) {
+  for(trial <- trials){
     var iqOut = Vector[Complex]()
-    for (iq <- trial.iqin) {
+    for(iq <- trial.iqin){
       poke(c.io.in.bits.iq, iq)
-      // wait until input is accepted
       var cyclesWaiting = 0
       while (!peek(c.io.in.ready) && cyclesWaiting < maxCyclesWait) {
         cyclesWaiting += 1
         if (cyclesWaiting >= maxCyclesWait) {
           expect(false, "waited for input too long")
         }
-        iqOut = getIQOut(c, iqOut)
+        iqOut = peek(c.io.out.bits.iq)
         step(1)
-      }
-      iqOut = getIQOut(c, iqOut)
-      peek(c.io.debug.powerHigh)
-      peek(c.io.debug.powerLow)
-      peek(c.io.debug.corrComp)
-      peek(c.io.debug.corrNum)
-      peek(c.io.debug.corrDenom)
-//      peek(c.io.debug.iq)
-      step(1)
-    }
-    // wait for remaining output after pushing in IQ data
-    poke(c.io.in.valid, 1)
-    poke(c.io.in.bits.iq, Complex(0, 0))
-    var cyclesWaiting = 0
-    while (cyclesWaiting < maxCyclesWait) {
-      cyclesWaiting += 1
-      peek(c.io.debug.powerHigh)
-      peek(c.io.debug.powerLow)
-      peek(c.io.debug.corrComp)
-      peek(c.io.debug.corrNum)
-      peek(c.io.debug.corrDenom)
-      //      peek(c.io.debug.iq)
-      iqOut = getIQOut(c, iqOut)
-      step(1)
-    }
-    peek(c.io.debug.powerHigh)
-    peek(c.io.debug.powerLow)
-    peek(c.io.debug.corrComp)
-    peek(c.io.debug.corrNum)
-    peek(c.io.debug.corrDenom)
-    //    peek(c.io.debug.iq)
-    iqOut = getIQOut(c, iqOut)
-    // set desired tolerance
-    // in this case, it's pretty loose (2 bits)
-    // can you get tolerance of 1 bit? 0? what makes the most sense?
-    fixTolLSBs.withValue(tolLSBs) {
-      // check every output where we have an expected value
-      if (trial.iqout.isEmpty) {
-        assert(iqOut.isEmpty, "No IQ should have been passed through")
-      } else {
-        val iqRef = trial.iqout.get
-        assert(iqOut.length == iqRef.length,
-               s"The packet length was ${iqOut.length} but should have been ${iqRef.length}")
-        iqOut.indices.foreach {
-         i => assert(iqOut(i) == iqRef(i), s"iq mismatch: ref ${iqRef(i)} != ${iqOut(i)} @$i")}
       }
     }
   }
@@ -97,18 +41,18 @@ class PacketDetectTester[T <: chisel3.Data](c: PacketDetect[T], trials: Seq[IQ],
 /**
   * Convenience function for running tests
   */
-object FixedPacketDetectTester {
-  def apply(params: FixedPacketDetectParams, trials: Seq[IQ]): Boolean = {
-    chisel3.iotesters.Driver.execute(Array("-tbn", "firrtl", "-fiwv"), () => new PacketDetect(params)) {
-      c => new PacketDetectTester(c, trials)
+object FixedCFOCorrectionTester {
+  def apply(params: FixedCFOCorrectionParams, trials: Seq[IQ]): Boolean = {
+    chisel3.iotesters.Driver.execute(Array("-tbn", "firrtl", "-fiwv"), () => new CFOCorrection(params)) {
+      c => new CFOTester(c, trials)
     }
   }
 }
 
-object RealPacketDetectTester {
-  def apply(params: PacketDetectParams[dsptools.numbers.DspReal], trials: Seq[IQ]): Boolean = {
-    chisel3.iotesters.Driver.execute(Array("-tbn", "verilator", "-fiwv"), () => new PacketDetect(params)) {
-      c => new PacketDetectTester(c, trials)
+object RealCFOCorrectionTester {
+  def apply(params: CFOCorrectionParams[dsptools.numbers.DspReal], trials: Seq[IQ]): Boolean = {
+    chisel3.iotesters.Driver.execute(Array("-tbn", "verilator", "-fiwv"), () => new CFOCorrection(params)) {
+      c => new CFOTester(c, trials)
     }
   }
 }
