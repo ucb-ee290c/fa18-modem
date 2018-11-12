38058e8fd2ef01a94fc4b95986aef7c98751c50b
sehuang <sehuang@berkeley.edu>
2018-11-12 15:37:53 -0800

    Moved SerialPacketBundle to Interface.scala

--- src/main/scala/Interface.scala
+++ src/main/scala/Interface.scala
@@ -52,3 +52,13 @@ object PacketBundle {
   def apply[T <: Data](params: PacketBundleParams[T]): PacketBundle[T] = new PacketBundle[T](params)
   def apply[T <: Data](size: Int, proto: DspComplex[T]): PacketBundle[T] = new PacketBundle[T](PacketBundleParams[T](size, proto))
 }
+
+
+ class SerialPacketBundle[T <: Data](val params: PacketBundleParams[T]) extends Bundle {
+   val pktStart: Bool = Bool()
+   val pktEnd: Bool = Bool()
+   val iq: DspComplex[T] = params.protoIQ
+ }
+ object SerialPacketBundle {
+   def apply[T <: Data](params: PacketBundleParams[T]): SerialPacketBundle[T] = new SerialPacketBundle(params)
+ }
