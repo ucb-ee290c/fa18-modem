8e61ccc3e5757a8db734283721b9a17e8b3912d6
Paul Kwon <hyunjaekwon@gmail.com>
2018-11-14 17:27:51 -0800

    SDF architecture FFT

--- src/test/scala/SerDesSpec.scala
+++ src/test/scala/SerDesSpec.scala
@@ -5,23 +5,27 @@ import org.scalatest.{FlatSpec, Matchers}
 import breeze.math.{Complex}
 import breeze.linalg.{randomDouble}
 
-class FixedDeserializerSpec extends FlatSpec with Matchers {
-  behavior of "FixedDeserializer"
+class SerDesSpec extends FlatSpec with Matchers {
+  behavior of "FixedSerDes"
 
-  val base_params = FixedDeserializerParams(
+  val base_params = FixedSerDesParams(
     dataWidth = 5,
-    deserRatio = 2,
+    ratio = 2,
     maxVal = 1,
   )
 
-  it should f"deserialize by 2" in {
+  it should f"serialize/deserialize by 2" in {
     val inp = Seq.fill(12) { Complex(randomDouble() * 2 - 1, randomDouble() * 2 - 1) }
+    FixedSerializerTester(base_params, inp) should be (true)
     FixedDeserializerTester(base_params, inp) should be (true)
+    FixedDesSerTester(base_params, inp) should be (true)
   }
 
-  it should f"deserialize by 64" in {
+  it should f"serialize/deserialize by 64" in {
     val inp = Seq.fill(64 * 4) { Complex(randomDouble() * 2 - 1, randomDouble() * 2 - 1) }
-    val params = base_params.copy(deserRatio = 64)
+    val params = base_params.copy(ratio = 64)
+    FixedSerializerTester(params, inp) should be (true)
     FixedDeserializerTester(params, inp) should be (true)
+    FixedDesSerTester(params, inp) should be (true)
   }
 }
