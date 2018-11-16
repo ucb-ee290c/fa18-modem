e1d08ed52c38aa484428a8de7797a9e7e082130d
Sean Huang <sehuang@bwrcrdsl-3.eecs.berkeley.edu>
2018-11-15 20:41:44 -0800

    All build errors fixed with bogus test vetor (need to change val trials). TEST WILL HANG

--- src/test/scala/ModemSpec.scala
+++ src/test/scala/ModemSpec.scala
@@ -38,9 +38,9 @@ class FixedRXSpec extends FlatSpec with Matchers {
 
   val fixedFFTParams = FixedFFTParams(dataWidth = iqWidth, binPoint = binPoint, numPoints = numPoints, twiddleWidth = iqWidth)
 
-  val hardBitsBundleParams = new BitsBundleParams[Bool]{
+  val hardBitsBundleParams = new BitsBundleParams[UInt]{
     val bitsWidth: Int = bitsWidth
-    val protoBits: Bool = Bool()
+    val protoBits: UInt = UInt(1.W)
   }
 
   val hardDemodParams = HardDemodParams(width = iqWidth, bitsWidth = bitsWidth)
