a60d65ff4281d4aab2588cd53c720e715b2f7bf0
sehuang <sehuang@berkeley.edu>
2018-11-15 18:47:25 -0800

    Made an RX tester without test methods yet

---
+++ src/test/scala/ModemTester.scala
@@ -0,0 +1,33 @@
+package modem
+
+import breeze.math.Complex
+import scala.math
+import dsptools.DspTester
+
+class RXTester[T <: Data, U <: Data](c: RX[T,U], trials: Seq(1)) extends DspTester(c) {
+  val maxCyclesWait = 1000 //Whatever numnber
+
+  poke(c.io.in.valid, 1)
+  poke(c.io.out.ready, 1)
+
+  for(trial <- trials) {
+
+  }
+}
+
+object FixedRXTester {
+  def apply(
+    iqParams: IQBundleParams[FixedPoint],
+    pktDetectParams: FixedPacketDetectParams,
+    eqParams: FixedEqualizerParams,
+    cfoParams: FixedCFOParams,
+    fftParams: FixedFFTParams,
+    bitsBundleParams: BitsBundleParams[Bool()],
+    demodParams: HardDemodParams,
+    viterbiParams: FixedCoding,
+    trials: Seq(1)): Boolean = {
+    chisel3.iotesters.Driver.execute(Array("-tbn", "firrtl", "-fiwv"), () => new RX(iqParams, pktDetectParams, eqParams, cfoParams, fftParams, bitsBundleParams, demodParams, viterbiParams) {
+      c => new RXTester(c, trials)
+    }
+  }
+}
