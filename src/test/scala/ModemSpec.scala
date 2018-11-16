a8c523cbe42e952bb0c7f7b1748c7d3b27b8593d
sehuang <sehuang@berkeley.edu>
2018-11-15 18:46:59 -0800

    Added CP params and receive ofdm test

--- src/test/scala/ModemSpec.scala
+++ src/test/scala/ModemSpec.scala
@@ -6,13 +6,15 @@ import breeze.math.{Complex}
 import breeze.signal.{fourierTr, iFourierTr}
 import breeze.linalg.{randomDouble}
 
-class FixedModemSpec extends FlatSpec with Matchers {
-  behavior of "FixedModem"
+class FixedRXSpec extends FlatSpec with Matchers {
+  behavior of "FixedRX"
 
   // These are  bogus placeholder numbers
   val iqWidth = 5
   val binPoint = iqWidth - 3
   val bitsWidth = 48
+  val prefixLength = 16
+  val symbolLength = 64
 
   val fixedIQParams = new IQBundleParams[FixedPoint](
       val protoIQ: DspComplex[T] = DspComplex(FixedPoint(iqWidth.W, binPoint.BP))
@@ -24,6 +26,12 @@ class FixedModemSpec extends FlatSpec with Matchers {
 
   val fixedCFOParams = FixedCFOParams(width = iqWidth, stagesPerCycle = 5)
 
+  val fixedCPParams = new CyclicPrefixParams[FixedPoint](
+    val protoIQ = DspComplex(FixedPoint(iqWidth.W, binPoint.BP))
+    val prefixLength = prefixLength
+    val symbolLength = symbolLength
+  )
+
   val fixedFFTParams = FixedFFTParams(dataWidth = iqWidth, binPoint = binPoint, twiddleWidth = iqWidth)
 
   val hardBitsBundleParams = new BitsBundleParams[Bool()](
@@ -34,4 +42,9 @@ class FixedModemSpec extends FlatSpec with Matchers {
   val hardDemodParams = HardDemodParams(width = iqWidth, bitsWidth = bitsWidth)
 
   val hardViterbiParams = FixedCoding()
+
+  it should "receive ofdm" in {
+    val trials = Seq(1)
+    FixedRXTester(fixedIQParams, fixedPktDetectParams, fixedEqualizerParams, fixedCFOParams, fixedCPParams, fixedFFTParams, hardBitsBundleParams, hardDemodParams, hardViterbiParams, trials) should be (true)
+  }
 }
