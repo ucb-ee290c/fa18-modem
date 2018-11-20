fb80df900c0342309e2a259a9a64aed97b243038
sehuang <sehuang@berkeley.edu>
2018-11-20 10:52:23 -0800

    DspTester Test Options

---
+++ src/test/scala/TestSetup.scala
@@ -0,0 +1,29 @@
+package modem
+
+import chisel3.iotesters._
+import dsptools._
+
+object TestSetup {
+  val testerOptionsFirrtl = TesterOptions(
+    isVerbose = false,
+    displayBase = 16,
+    backendName = "firrtl"
+  )
+  val testerOptionsVerilog = TesterOptions(
+    isVerbose = false,
+    displayBase = 16,
+    backendName = "verilator"
+  )
+  val dspTesterOptionsVerilog = new DspTesterOptionsManager {
+    dspTesterOptions = dspTesterOptions.copy(
+      isVerbose = false
+    )
+    testerOptions = testerOptionsVerilog
+  }
+  val dspTesterOptions = new DspTesterOptionsManager {
+    dspTesterOptions = dspTesterOptions.copy(
+      isVerbose = false
+    )
+    testerOptions = testerOptionsFirrtl
+  }
+}
