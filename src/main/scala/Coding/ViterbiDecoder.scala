fb084112f87ade23786838a1bc1f8eaca9dc994e
Sean Huang <sehuang@bwrcrdsl-3.eecs.berkeley.edu>
2018-11-26 11:43:28 -0800

    in.ready is not an input

--- src/main/scala/Coding/ViterbiDecoder.scala
+++ src/main/scala/Coding/ViterbiDecoder.scala
@@ -26,17 +26,18 @@ class ViterbiDecoder[T <: Data: Real](params: CodingParams[T]) extends Module {
   val tracebackModule     = Module(new Traceback[T](params))
 
   DePuncturingModule.io.in_hard :=  io.in.bits.bits
-  DePuncturingModule.io.inReady := io.in.ready
+  DePuncturingModule.io.inReady := io.in.valid
   DePuncturingModule.io.stateIn := 0.U
   io.out_dp := DePuncturingModule.io.out_hard
 
   pathMetricModule.io.in      <> DePuncturingModule.io.out_hard
-  pathMetricModule.io.inReady := io.in.ready
+  pathMetricModule.io.inReady := io.in.valid
   io.out_pm <> pathMetricModule.io.outPM
   io.out_sp <> pathMetricModule.io.outSP
 
   tracebackModule.io.inPM     := pathMetricModule.io.outPM
   tracebackModule.io.inSP     := pathMetricModule.io.outSP
-  tracebackModule.io.inReady  := io.in.ready
+  tracebackModule.io.inReady  := io.in.valid
   io.out  <> tracebackModule.io.out
+  io.in.ready := io.out.ready
 }
