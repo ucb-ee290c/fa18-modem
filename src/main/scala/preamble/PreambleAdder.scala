a85c06f521737ef69ca67e560ba755f496e0e631
sehuang <sehuang@berkeley.edu>
2018-11-30 22:13:15 -0800

    Wrote Preamble Adder and tests

--- src/main/scala/preamble/PreambleAdder.scala
+++ src/main/scala/preamble/PreambleAdder.scala
@@ -30,5 +30,40 @@ case class FixedPreambleParams(
   val protoIQ = DspComplex(FixedPoint(iqWidth.W, (iqWidth-3).BP)).cloneType
 }
 
-class PreambleAdder[T<:Data] extends Module {
-  val io = IO(
+class PreambleAdder[T<:Data](val params: PreambleParams[T]) extends Module {
+  val io = IO( PreambleAdderIO(params) )
+
+  val inputBuffer = (0 until (params.stLength + params.ltLength)).foldLeft(io.in.bits.iq(0)){(prev, cur) => RegNext(prev)}
+  // val stfVec = VecInit(IEEE80211.stf)
+  // val ltfVec = VecInit(IEEE80211.ltf)
+  val preambleVec = VecInit(IEEE80211.stf :+ IEEE80211.ltf)
+
+  val idle :: preamble :: data :: Nil
+  val nxtState = Wire(UInt(2.W))
+  val curState = RegNext(nxtState, idle)
+  val preambleCounter = Counter(params.stLength + params.ltLength)
+
+  io.out.bits.valid := false.B
+  io.out.bits.iq.real := ConvertableTo[T].fromDouble(0.0)
+  io.out.bits.iq.imag := ConvertableTo[T].fromDouble(0.0)
+  switch(curState){
+    is(idle){
+      preambleCounter.value := 0.U
+      io.out.bits.valid := false.B
+      nxtState := Mux(io.in.bits.pktStart, preamble, idle)
+    }
+    is(preamble){
+      io.out.bits.valid := true.B
+      io.out.bits.iq := preambleVec(preambleCounter.value)
+      when(io.out.bits.ready){
+        nxtState := Mux(preambleCounter.inc(), data, preamble)
+      }.otherwise{
+        nxtState := preamble
+      }
+    }
+    is(data){
+      io.out.bits.valid ;= true.B
+      io.out.bits.iq := inputBuffer
+      nxtState := Mux(io.in.bits.pktEnd, idle, data)
+    }
+  }
---
+++ src/test/scala/PreambleTester.scala
@@ -0,0 +1,46 @@
+package modem
+
+import scala.math._
+import breeze.math.Complex
+import breeze.numerics.{cos, sin, sqrt}
+import dsptools.DspTester
+
+/**
+  * Case class holding information needed to run an individual test
+  */
+
+/**
+  * DspTester for CFO
+  *
+  * Run each trial in @trials
+  */
+class PreambleTester[T <: chisel3.Data](c: PreambleAdder[T], trials: Seq[IQ], tolLSBs: Int = 2) extends DspTester(c) {
+  val maxWaitCycles = 100
+  poke(c.io.in.valid, 1)
+  poke(c.io.out.ready, 1)
+  for (trial <- trials){
+    poke(c.io.in.bits.pktStart, 1)
+    for(iq <- trial.iniq){
+      poke(c.io.in.bits.pktStart, 0)
+      poke(c.io.in.bits.iq(0), iq)
+      peek(c.io.out.bits.iq)
+      step(1)
+    }
+    (0 until 320).foreach{
+      peek(c.io.out.bits.iq)
+      step(1)
+      }
+    poke(c.io.in.bits.pktEnd, 1)
+  }
+}
+
+/**
+  * Convenience function for running tests
+  */
+object FixedPreambleTester {
+  def apply(params: FixedPreambleParams, trials: Seq[IQ], tolLSBs: Int=3): Boolean = {
+    chisel3.iotesters.Driver.execute(Array("-tbn", "verilator", "-fiwv"), () => new PreambleTester(params)) {
+      c => new PreambleTester(c, trials, reals, cfo, tolLSBs)
+    }
+  }
+}
